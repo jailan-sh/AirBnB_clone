1a01fd611720929aaf9e0cec72676f0a925d59a4
jailan-sh <jilan.elsherbiny@gmail.com>
2023-08-12 12:33:32 -0700

    edit error

--- console.py
+++ console.py
@@ -148,28 +148,6 @@ class HBNBCommand(cmd.Cmd):
                 setattr(obj, obj_attr, attr_value)
             storage.save()
 
-    def default(self, line):
-        """ to run line """
-        args = line.split(".")
-        if args[0] in self.__cls:
-            if args[1] == "all()":
-                self.do_all(args[0])
-            elif args[1] == "count()":
-                count = [v for k, v in storage.all().items()
-                         if k.startswith(args[0])]
-                print(len(count))
-            elif args[1].startswith("show"):
-                _id = args[1].split('"')[1]
-                self.do_show(f"{args[0]} {_id}")
-            elif args[1].startswith("destroy"):
-                _id = args[1].split('"')[1]
-                self.do_destroy(f"{args[0]} {_id}")
-            elif args[1].startswith("update"):
-                _id = args[1].split('"')[1]
-                _attr = args[1].split('"')[3]
-                _value = args[1].split('"')[5]
-                self.do_update(f"{args[0]} {_id} {_attr} {_value}")
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
