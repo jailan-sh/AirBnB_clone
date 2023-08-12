b8839617cd79af1559e923e2fedcd5b0df786881
ghaidaareda <dodo.sa7afy@gmail.com>
2023-08-12 11:40:54 -0700

    str_test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -21,11 +21,17 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(datetime, type(BaseModel().updated_at))
         self.assertIn(BaseModel(), models.storage.all().values())
     def test_different_values(self):
+        """ test different values for different objects"""
         bm_1 = BaseModel()
         sleep (0.02)
         bm_2 = BaseModel()
         self.assertNotEqual(bm_1.id, bm_2.id)
         self.assertNotEqual(bm_1.created_at, bm_2.created_at)
         self.assertNotEqual(bm_1.updated_at, bm_2.updated_at)
+    def test_str(self):
+        """ test str method """
+        bm_3 = BaseModel()
+        self.assertEqual(str(bm_3), "[BaseModel] ({}) {}".format(bm_3.id,
+                                                                 bm_3.__dict__))
 if __name__ == '__main__':
     unittest.main()
