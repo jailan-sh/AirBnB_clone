4c6af39b6a1a3aa7c6d621c0c4d92fd037996e76
ghaidaareda <dodo.sa7afy@gmail.com>
2023-08-12 09:36:52 -0700

    engine_test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,3 +3,21 @@
 tests for class Storage
 """
 import unittest
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.base_model import BaseModel
+
+class TestFileStorage(unittest.TestCase):
+    """ unittest for FileStorage """
+    def test_no_args(self):
+        """ test no args """
+        self.assertEqual(FileStorage, type(FileStorage()))
+    def test_def_value(self):
+        """ test values"""
+        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
+        self.assertEqual(type(FileStorage._FileStorage__objects), dict)
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -3,3 +3,13 @@
 tests for class State
 """
 import unittest
+from models.state import State
+
+class TestState(unittest.TestCase):
+    """ unittest for state """
+    def test_no_args(self):
+        self.assertEqual(State, type(State()))
+    def test_def_value(self):
+        """ test values"""
+        state = State()
+        self.assertEqual(state.name, "")
