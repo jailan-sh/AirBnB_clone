18331ad01694b929fffa857bfd17069116387800
ghaidaareda <dodo.sa7afy@gmail.com>
2023-08-12 10:00:13 -0700

    base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,11 +5,19 @@ test for instantiation, save & to_dict
 """
 from models.base_model import BaseModel
 import unittest
+from datetime import datetime
+import models
 
 class TestBaseModel(unittest.TestCase):
     """ unittest for BaseModel """
 
     def test_no_args(self):
         self.assertEqual(BaseModel, type(BaseModel()))
+    def test_def_value(self):
+        """ test values"""
+        self.assertEqual(str, type(BaseModel().id))
+        self.assertEqual(datetime, type(BaseModel().created_at))
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+        self.assertIn(BaseModel(), models.storage.all().values())
 if __name__ == '__main__':
     unittest.main()
