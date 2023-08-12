89eb5eb94bb77cece433086212359e5e45483dc7
ghaidaareda <dodo.sa7afy@gmail.com>
2023-08-12 07:33:02 -0700

    city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,3 +4,14 @@ This is a unittest for city.py
 test for instantiation, save & to_dict
 """
 import unittest
+from models.city import City
+
+class TestCity(unittest.TestCase):
+    """ unittest for city """
+    def test_no_args(self):
+        self.assertEqual(City, type(City()))
+    def test_def_value(self):
+        """ test values"""
+        city = City()
+        self.assertEqual(city.state_id, "")
+        self.assertEqual(city.name, "")
